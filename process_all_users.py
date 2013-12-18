c1b551beb0ce267eddeee0831a4a3891638d3fe0
Tony Vashevko <avashevko@gmail.com>
2013-12-17 18:59:39 -0800

    taking account of barley limits

--- process_all_users.py
+++ process_all_users.py
@@ -84,8 +84,8 @@ def process_barley(outfiles="../data/out"):
         current_script += barley_addend.format(user=user, count=ucount, outdir=outfiles)
         submitted_count += ucount
 
-        print('-'*80)
-        print(current_script)
+        # print('-'*80)
+        # print(current_script)
 
         if submitted_count > (float(total) / BARLEY_MAX_JOBS):
             qsubfile = open('submit.script',mode='w')
