d10354a5b6f24eb085b3de4c3713d5b4a30594b4
Tony Vashevko <avashevko@gmail.com>
2014-01-23 23:37:26 -0800

    mode

--- predict-trading.R
+++ predict-trading.R
@@ -101,7 +101,7 @@ day.stats <- function(c.day,u.alts,u.fpt,alts.fpt,u.dbap) {
 
 # run settings
 SAMP.FRAC <- 1.0
-FUNC.NAME <- 'day.stats'
+FUNC.NAME <- 'success.by.currency'
 
 # system specific settings
 hostname <- Sys.info()['nodename']
