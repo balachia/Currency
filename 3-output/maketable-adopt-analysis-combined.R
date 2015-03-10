7d122b1fa10c29d3049dc8750243f1efca534007
Tony Vashevko <avashevko@gmail.com>
2015-03-10 15:08:37 -0700

    make coef names et al correct

--- 3-output/maketable-adopt-analysis-combined.R
+++ 3-output/maketable-adopt-analysis-combined.R
@@ -3,7 +3,8 @@ library(texreg)
 library(stargazer)
 
 # source utilities
-source('table-utilities.R')
+setwd('~/2YP/code')
+source('3-output/table-utilities.R')
 
 setwd('~/Data/forex')
 #load('Rdata/adopt-analysis-sudocox-user.Rdata')
@@ -56,86 +57,131 @@ addBelowHTML <- function(x,rows=NULL) {
 ############################################################
 # just user effects
 
-basemodel <- function(f,...) {
-    f(l=userms['basem10.base', 'basem10.l', 'basem10.10'],
-      label='tab:',
+wrap <- function(f,...) {
+    f(l=userms[c('basem10.base','basem10.l','basem10.10')],
+      label='tab:adopts-short-user',
       digits=3, float.pos='htb',
-      caption='Conditional Logit: Chance of Currency Adoption',
+      caption='Conditional Logit (Trader/Day): Chance of Currency Adoption',
       dcolumn=TRUE, booktabs=TRUE, use.packages=FALSE,
       doctype=FALSE, star.symbol='&lowast;',
       include.rsquared=FALSE, include.maxrs=FALSE, include.missings=FALSE,
       custom.model.names=c('(1) Baseline','(2) Log Rank','(3) Rank > 10'),
       reorder.coef=NULL,
-      custom.coef.names='',
+      custom.coef.names=c('Alter Trade','Alter Profit',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Rank','Trade*Rank','Profit*Rank'),
       ...)
 }
+
+texstr <- wrap(texreg)
+htmlstr <- wrap(htmlreg)
+htmlstr <- strip.html(htmlstr)
+
+write(texstr, file='adopts-short-sudocox-user.tex')
+write(htmlstr, file='adopts-short-sudocox-user.html')
 
 
 ############################################################
 # just cp-friends effects
 
-basemodel <- function(f,...) {
-    f(l=list(basem10.base, basem10.l, basem10.10),
+wrap <- function(f,...) {
+    f(l=cpfrms[c('basem11b.base','basem11b.l','basem11b.10')],
       label='tab:',
       digits=3, float.pos='htb',
-      caption='Conditional Logit: Chance of Currency Adoption',
+      caption='Conditional Logit (Currency Pair/Day): Chance of Currency Adoption',
       dcolumn=TRUE, booktabs=TRUE, use.packages=FALSE,
       doctype=FALSE, star.symbol='&lowast;',
       include.rsquared=FALSE, include.maxrs=FALSE, include.missings=FALSE,
-      custom.model.names=c('(1) Baseline','(2) Log Rank','(3) Rank > 10'),
+      custom.model.names=c('(4) Baseline','(5) Log Rank','(6) Rank > 10'),
       reorder.coef=NULL,
-      custom.coef.names='',
+      custom.coef.names=c('Alter Trade','Alter Profit',
+                          'Friends','Trade*Friends','Profit*Friends',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends'),
       ...)
 }
+
+texstr <- wrap(texreg)
+htmlstr <- wrap(htmlreg)
+htmlstr <- strip.html(htmlstr)
+
+write(texstr, file='adopts-short-sudocox-cp-friends.tex')
+write(htmlstr, file='adopts-short-sudocox-cp-friends.tex')
 
 
 ############################################################
 # both cp effects
 
-basemodel <- function(f,...) {
-    f(l=list(basem10.base, basem10.l, basem10.10),
+wrap <- function(f,...) {
+    f(l=c(cpms[c('basem11.base','basem11.l','basem11.10')],cpfrms[c('basem11b.base','basem11b.l','basem11b.10')]),
       label='tab:',
       digits=3, float.pos='htb',
-      caption='Conditional Logit: Chance of Currency Adoption',
+      caption='Conditional Logit (Currency Pair/Day): Chance of Currency Adoption',
       dcolumn=TRUE, booktabs=TRUE, use.packages=FALSE,
       doctype=FALSE, star.symbol='&lowast;',
       include.rsquared=FALSE, include.maxrs=FALSE, include.missings=FALSE,
-      custom.model.names=c('(1) Baseline','(2) Log Rank','(3) Rank > 10'),
+      custom.model.names=c('(4) Baseline','(5) Log Rank','(6) Rank > 10', '(7) Baseline', '(8) Log Rank', '(9) Rank > 10'),
       reorder.coef=NULL,
-      custom.coef.names='',
+      omit.coef='^Rank$',
+      custom.coef.names=c('Alter Trade','Alter Profit',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Friends','Trade*Friends','Profit*Friends',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends'),
       ...)
 }
+
+texstr <- wrap(texreg)
+htmlstr <- wrap(htmlreg)
+
+#rows <- list(c('Trader FE',rep(c('N','N'),each=3)),
+#             c('Currency Pair FE',rep(c('N','Y'),each=3)))
+#addBelowTex(texstr,rows)
+#addBelowHTML(htmlstr,rows)
+
+htmlstr <- strip.html(htmlstr)
+
+write(texstr, file='adopts-short-sudocox-cp-all.tex')
+write(htmlstr, file='adopts-short-sudocox-cp-all.html')
 
 
 ############################################################
 # user and cp effects
 
-basemodel <- function(f,...) {
-    f(l=list(basem10.base, basem10.l, basem10.10),
+wrap <- function(f,...) {
+    f(l=c(userms[c('basem10.base','basem10.l','basem10.10')],cpfrms[c('basem11b.base','basem11b.l','basem11b.10')]),
       label='tab:',
       digits=3, float.pos='htb',
       caption='Conditional Logit: Chance of Currency Adoption',
       dcolumn=TRUE, booktabs=TRUE, use.packages=FALSE,
       doctype=FALSE, star.symbol='&lowast;',
       include.rsquared=FALSE, include.maxrs=FALSE, include.missings=FALSE,
-      custom.model.names=c('(1) Baseline','(2) Log Rank','(3) Rank > 10'),
+      custom.model.names=c('(1) Baseline','(2) Log Rank','(3) Rank > 10', '(4) Baseline', '(5) Log Rank', '(6) Rank > 10'),
       reorder.coef=NULL,
-      custom.coef.names='',
+      custom.coef.names=c('Alter Trade','Alter Profit',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Rank','Trade*Rank','Profit*Rank',
+                          'Friends','Trade*Friends','Profit*Friends',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends',
+                          'Rank*Friends','Trade*Rank*Friends','Profit*Rank*Friends'),
       ...)
 }
 
+texstr <- wrap(texreg)
+htmlstr <- wrap(htmlreg)
 
+rows <- list(c('Trader FE',rep(c('Y','N'),each=3)),
+             c('Currency Pair FE',rep(c('N','Y'),each=3)))
+addBelowTex(texstr,rows)
+addBelowHTML(htmlstr,rows)
 
+htmlstr <- strip.html(htmlstr)
 
-
-
-
-
-
-
-
-
-
+write(texstr, file='adopts-short-sudocox-all.tex')
+write(htmlstr, file='adopts-short-sudocox-all.html')
 
 
 
